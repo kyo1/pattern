3280990b7b21b952c4ab5224beec5e7616cb5cab
kyo1 <kyo112358@gmail.com>
2020-04-25 17:43:47 +0900

    Use kong

--- cmd/pattern/main.go
+++ cmd/pattern/main.go
@@ -2,64 +2,46 @@ package main
 
 import (
 	"fmt"
+	"github.com/alecthomas/kong"
 	"github.com/kyo1/pattern"
-	"github.com/urfave/cli"
 	"log"
-	"os"
 	"strconv"
 	"strings"
 )
 
+var cli struct {
+	Create struct {
+		Length int `arg name:"length" help:"length of the pattern to be generated" default:"0"`
+	} `cmd help:"generate a cyclic pattern"`
+
+	Offset struct {
+		BigEndian bool `default:"false"`
+
+		Needle string `arg name:"needle" default:""`
+	} `cmd help:"find the offset"`
+}
+
 func main() {
-	app := cli.NewApp()
+	ctx := kong.Parse(&cli)
+	switch ctx.Command() {
+	case "create <length>":
+		fmt.Println(pattern.Create(cli.Create.Length))
+	case "offset <needle>":
+		if strings.HasPrefix(cli.Offset.Needle, "0x") {
+			x, err := strconv.ParseUint(cli.Offset.Needle, 0, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			cli.Offset.Needle = pattern.Hex2str(x, cli.Offset.BigEndian)
 
-	app.Commands = []*cli.Command{
-		{
-			Name:    "create",
-			Aliases: []string{"c"},
-			Usage:   "generate a cyclic pattern",
-			Action: func(c *cli.Context) error {
-				length, err := strconv.ParseInt(c.Args().First(), 0, 0)
-				if err != nil {
-					log.Fatal(err)
-				}
-				fmt.Println(pattern.Create(int(length)))
-				return nil
-			},
-		},
-		{
-			Name:    "offset",
-			Aliases: []string{"o", "search", "s"},
-			Usage:   "find the offset",
-			Flags: []cli.Flag{
-				&cli.BoolFlag{
-					Name:    "big",
-					Aliases: []string{"b"},
-					Usage:   "using big endian",
-				},
-			},
-			Action: func(c *cli.Context) error {
-				needle := c.Args().First()
-				if strings.HasPrefix(needle, "0x") {
-					x, err := strconv.ParseUint(needle, 0, 64)
-					if err != nil {
-						log.Fatal(err)
-					}
-					needle = pattern.Hex2str(x, c.Bool("big"))
-				}
-				offset := pattern.Offset(needle)
-				if offset != -1 {
-					fmt.Println(needle, "found at offset", offset)
-				} else {
-					fmt.Println("not found")
-				}
-				return nil
-			},
-		},
-	}
-
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
+		}
+		offset := pattern.Offset(cli.Offset.Needle)
+		if offset != -1 {
+			fmt.Println(cli.Offset.Needle, "found at offset", offset)
+		} else {
+			fmt.Println("not found")
+		}
+	default:
+		panic(ctx.Command)
 	}
 }
